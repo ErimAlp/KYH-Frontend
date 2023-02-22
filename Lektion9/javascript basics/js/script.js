c05fbbb183597b2c060ce6aebae8deee6033d4c6
Erim <erimby@hotmail.com>
2023-02-22 13:48:14 +0100

    Lektion9 - Javascript (Basics)

--- Lektion9/javascript basics/js/script.js
+++ Lektion9/javascript basics/js/script.js
@@ -1,3 +1,11 @@
 //DOM manipulation
 
-document.getElementById("app").innerText = "ERIM"
+var firstName = document.getElementById("firstName").value 
+var lastName = document.getElementById("lastName").value
+
+console.log(firstName)
+
+console.log(document.getElementsByName('firstName')[0].value)
+console.log(document.getElementsByTagName('input'))
+
+console.log(document.getElementById('displayName').innerHTML = "<strong> Erim Bayramoglu </strong>")
