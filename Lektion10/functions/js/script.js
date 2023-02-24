1df09d408774ecbd84e322b0ffcf8508057dd1e1
Erim <erimby@hotmail.com>
2023-02-24 12:37:44 +0100

    Lektion10 - Javascript

--- Lektion10/functions/js/script.js
+++ Lektion10/functions/js/script.js
@@ -1,22 +1,38 @@
 /*
-    function    =   fristående och globalt deklarerad
+    function    =   fristående och gobalt deklarerad
     metod       =   ingår i en instansierad klass
-    const       =   statisk deklarerad - dvs global
-
+    const       =   statisk deklarerad - dvs gobal
     DEKLARERING AV FUNKTIONER
-    -------------------------------------------------------------------------------------------------------------------------------------
-    function(){}            =   anonymous function
-    function name() {}      =   function 
-
-    () => {}                = anonymous arrow function (ES6)
-    const name = () => {}   =   arrow function (ES 5)
-
+    --------------------------------------------------
+    function() {}               =   anonymous function
+    function name() {}          =   function
+    () => {}                    =   anonymous arrow function (ES6)
+    const name = () => {}       =   arrow function (ES 6)
+    function welcomeMessage(name) {
+        console.log(`välkommen ${name}`)
+    }
+    const welcomeMessage = (name) => {
+        console.log(`välkommen ${name}`)
+    }
+    const welcomeMessage = (name) => console.log(`välkommen ${name}`)
+    function doWork() {
+        console.log("sparar saker till databasen...")
+    }
+    const doWork = () => { 
+        console.log("sparar saker till databasen...") 
+    }
+    const doWork = () => console.log("sparar saker till databasen...")
+    function getDisplayName(firstName, lastName) {
+        return `${firstName} ${lastName}`
+    }
+    const getDisplayName = (firstName, lastName) => {
+        return `${firstName} ${lastName}`
+    }
+    const getDisplayName = (firstName, lastName) => `${firstName} ${lastName}`
 */
 
-function welcomeMessage (name){
-    console.log('Welcome $(name)')
-}
+const getDisplayName = (firstName, lastName) => `${firstName} ${lastName}`
 
-const welcomeMessage1 = (name) {
-    console.log(`Welcome $(name)`)
-}
+
+let displayName = getDisplayName("Hans", "Mattin-Lassei")
+console.log(displayName)
